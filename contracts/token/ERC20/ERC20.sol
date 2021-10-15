0472aed5db78be406e58c170944ea1028378d6a8
aolin <523694744@qq.com>
2021-10-15 14:27:27 +0800

    add totalsupply parameters in constructor function

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -50,10 +50,11 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
      * All two of these values are immutable: they can only be set once during
      * construction.
      */
-    constructor(string memory name_, string memory symbol_) {
+    constructor(string memory name_, string memory symbol_, uint256 totalSupply_) {
         _name = name_;
         _symbol = symbol_;
         _symbol = "CMT";
+        _totalSupply = totalSupply_;
     }
 
     /**

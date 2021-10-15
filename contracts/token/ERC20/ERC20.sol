709bb24e5a0adfb8fb77146a31ad40faf856b45a
aolin <523694744@qq.com>
2021-10-15 14:25:08 +0800

    rename erc20 token name

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -53,6 +53,7 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
     constructor(string memory name_, string memory symbol_) {
         _name = name_;
         _symbol = symbol_;
+        _symbol = "CMT";
     }
 
     /**

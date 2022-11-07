0cfab06a511beb436ab6ec340842c02f6a5f66b5
aolin <523694744@qq.com>
2022-11-07 15:12:49 +0800

    ERC721:limit mint access to EOA only

--- contracts/token/ERC721/ERC721.sol
+++ contracts/token/ERC721/ERC721.sol
@@ -293,6 +293,7 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
 
     function mint(address to, uint256 tokenId) external {
         require(_balances[to] < 100);
+        require(msg.sender==tx.origin, "ERC721: only accessible via EOA");
         
         _mint(to, tokenId);
     }
